98ac7417bb699a63934edb5b638e84c15918413f
ioetbc <ioetbc@gmail.com>
2022-11-23 18:47:34 +0000

    getting the images from unsplash and posts from reddit

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,28 +1,64 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useState } from 'react';
 import './Newtab.css';
 import './Newtab.scss';
-import { Text } from '@chakra-ui/react'
+import { Box, Heading, Image } from '@chakra-ui/react';
+import { useEffect } from 'react';
+var pos = require('pos');
+
 const Newtab = () => {
+  const getData = async () => {
+    const data = await fetch('https://www.reddit.com/r/UpliftingNews.json');
+    const json = await data.json();
+
+    console.log('son.data.children', json.data.children);
+
+    const title =
+      json.data.children[Math.floor(Math.random() * json.data.children.length)]
+        .data.title;
+
+    const titleClean = title.replace(/[^a-zA-Z ]/g, '');
+
+    var words = new pos.Lexer().lex(titleClean);
+
+    var tagger = new pos.Tagger();
+    var taggedWords = tagger.tag(words);
+
+    const gmm = taggedWords.filter((word) => word[1] === 'NNP');
+
+    const noun = gmm[0][0];
+
+    const image = await fetch(
+      `https://api.unsplash.com/search/photos?client_id=4R_WFGZxzdEW5imhr_k-fnpSPgJINGGTIMnMNKuaLkU&page=1&query=${noun}&count=1`
+    );
+    const jsonImage = await image.json();
+
+    console.log('jsonImage', jsonImage.results.length);
+    const randomIndex = Math.floor(Math.random() * jsonImage.results.length);
+
+    console.log(jsonImage.results[randomIndex].urls.raw);
+
+    return {
+      title,
+      noun,
+      image: jsonImage.results[randomIndex].urls.raw,
+    };
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData().then((data) => {
+      console.log(data);
+      setData(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Text fontSize='20px' color='white'>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </Text>
-        <Text fontSize='50px' color='tomato'>
-          PLANES
-        </Text>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <Heading position="absolute">{data.title}</Heading>
+      <Box width="100vw" height="100vh">
+        <Image width="100%" height="100%" src={data.image} alt={data.title} />
+      </Box>
     </div>
   );
 };
